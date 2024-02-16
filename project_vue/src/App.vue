222d4b781ec80a570943051192308d1f45385a2d
Luiz Bessa <luiz-augustosouza@hotmail.com>
2024-02-16 06:40:56 +0000

    Adding components with a short live cycle to test.

--- project_vue/src/App.vue
+++ project_vue/src/App.vue
@@ -2,15 +2,18 @@
   <div>
     <first-component-vue />
     <p>O dado de app é {{ Testando }} .</p>
+    <live-cycle-vue />
   </div>
 </template>
 <script>
+import LiveCycleVue from "./components/LiveCycle.vue";
 import FirstComponentVue from "./FirstComponent.vue";
 
 export default {
   name: "App",
   components: {
     FirstComponentVue,
+    LiveCycleVue,
   },
   data() {
     return {
---
+++ project_vue/src/components/LiveCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LiveCycle",
+  data() {
+    return {
+      nome: "I dont know ",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.nome = "Matheus";
+    }, 1111);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.nome = "Pedro";
+    }, 2111);
+  },
+};
+</script>
