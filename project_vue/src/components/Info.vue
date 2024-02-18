724423408d9f45a6c195783ea389e90c5a274e05
Luiz Bessa <luiz-augustosouza@hotmail.com>
2024-02-18 07:01:02 +0000

    Testando logica das diretivas do vue.js (V-IF,V-ELSE E V-SHOW).

--- project_vue/src/components/Info.vue
+++ project_vue/src/components/Info.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="Esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oprtunidades.</p>
     <p>Utilizo as sequintes tecnologias:</p>
     <ul>
       <li>JavaScript</li>
       <li>PHP</li>
       <li>Python</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 <script>
 export default {
   name: "Infoo",
+  data() {
+    return {
+      Esta_trabalhando: false,
+      mostrar_email: true,
+      email: "luiz-augustosouza@hotmail.com",
+    };
+  },
 };
 </script>
