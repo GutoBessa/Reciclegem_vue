9d860ff27f1909687223d6a72e9679d793a919fc
unknown <luiz-augustosouza@hotmail.com>
2024-02-24 07:16:47 +0000

    Adding a very interesting Vue.js feature to the project, with the idea of running more than one event at the same time.

--- project_vue/src/FirstComponent.vue
+++ project_vue/src/FirstComponent.vue
@@ -2,11 +2,16 @@
   <div>
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{ nome }} e trabalho como {{ Profission }} .</p>
+    <multiplos-eventos />
   </div>
 </template>
 <script>
+import MultiplosEventos from "./MultiplosEventos.vue";
 export default {
   name: "FirstComponent",
+  components: {
+    MultiplosEventos,
+  },
   data() {
     return {
       nome: "Luiz",
